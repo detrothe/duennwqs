9812d53d167e63005c1fa93671c2e507a8810e79
detlef <betonkanu@web.de>
2022-11-04 13:25:48 +0100

    second

--- src/utility.js
+++ src/utility.js
@@ -33,7 +33,7 @@ export function testNumber(wert, zeile, spalte, id) {
 export function SDuennTruss() {
     this.emodul = null
     this.Iy = null
-    this.r = [1,1]
+    this.r = [1, 1]
 }
 
 //------------------------------------------------------------------------------------------------
@@ -42,38 +42,42 @@ export function sichtbar(displayName) {
 
     if (displayName === 'eingabe') {
         document.getElementById("eingabe").style.display = "block";
+        document.getElementById("knotentabelle").style.display = "block";
+        document.getElementById("elementtabelle").style.display = "block";
+        document.getElementById("eingabe_duennQ_top").style.display = "block";
+        document.getElementById("duennQ_save_daten").style.display = "block";
     } else {
         document.getElementById("eingabe").style.display = "none";
+        document.getElementById("knotentabelle").style.display = "none";
+        document.getElementById("elementtabelle").style.display = "none";
+        document.getElementById("eingabe_duennQ_top").style.display = "none";
+        document.getElementById("duennQ_save_daten").style.display = "none";
     }
 
     if (displayName === 'ergebnisse') {
         document.getElementById("ergebnisse").style.display = "block";
-    } else {
-        document.getElementById("ergebnisse").style.display = "none";
-    }
-    if (displayName === 'duennQ') {
-        document.getElementById("knotentabelle").style.display = "block";
-        document.getElementById("elementtabelle").style.display = "block";
         document.getElementById("querschnittswerte").style.display = "block";
-        document.getElementById("eingabe_duennQ_top").style.display = "block";
-        document.getElementById("duennQ_save_daten").style.display = "block";
+
         document.getElementById("spannung_mxp").style.display = "block";
         document.getElementById("spannung_mxs").style.display = "block";
         document.getElementById("normalspannung").style.display = "block";
         document.getElementById("vergleichsspannung").style.display = "block";
         document.getElementById("schubspannung").style.display = "block";
-        document.getElementById("my-svg").style.display = "block";
     } else {
-        document.getElementById("knotentabelle").style.display = "none";
-        document.getElementById("elementtabelle").style.display = "none";
+        document.getElementById("ergebnisse").style.display = "none";
         document.getElementById("querschnittswerte").style.display = "none";
-        document.getElementById("eingabe_duennQ_top").style.display = "none";
-        document.getElementById("duennQ_save_daten").style.display = "none";
+
         document.getElementById("spannung_mxp").style.display = "none";
         document.getElementById("spannung_mxs").style.display = "none";
         document.getElementById("normalspannung").style.display = "none";
         document.getElementById("vergleichsspannung").style.display = "none";
         document.getElementById("schubspannung").style.display = "none";
+    }
+    if (displayName === 'duennQ') {
+
+        document.getElementById("my-svg").style.display = "block";
+    } else {
+
         document.getElementById("my-svg").style.display = "none";
     }
 }
